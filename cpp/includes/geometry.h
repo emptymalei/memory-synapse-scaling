1806861f3d7900275a069ccd1205d983974f71c8
OctoMiao <emptymalei@qq.com>
2017-10-29 01:29:27 -0600

    testing the three arrays

--- cpp/includes/geometry.h
+++ cpp/includes/geometry.h
@@ -1,9 +1,59 @@
 //
 // Created by Lei Ma on 10/28/17.
+// At this point this header file is not used.
+// The classes here are not tested.
 //
 
 #ifndef CPP_GEOMETRY_H
 #define CPP_GEOMETRY_H
+
+class WeightArray {
+public:
+   WeightArray(const int rown, const int coln) {
+      double**elem;
+      elem = new double*[rown];
+      for(int i=0;i != rown; ++i){
+         elem[i] = new double[coln];
+      }
+
+      int r = rown;
+      int c = coln;
+
+    } // Constructor
+   //  double* operator[](const int rown)  // getter
+   //  { 
+   //     return elem[rown];
+   //  }
+   double** matrix(){
+      return elem;
+   }
+   int row() { return r;}  // return size of the array of states
+   int column() { return c;}  // return size of the array of states
+   void init(const double iv = 0.0) // initialize the array to 0's
+   {
+      for(int i=0;i<r;i++) { // Might need long int when the array size gets huge!
+         for(int j=0;j<c;j++) {
+            elem[i][j] = iv;
+         }
+      }
+   }
+   
+   ~WeightArray()
+   {
+      int rown = r;
+      int coln = c;
+
+      for (int i = 0; i != rown; ++i){
+         delete[] elem[i];
+      }
+
+     delete[] elem;
+   }
+private:
+   double** elem;
+   int r;
+   int c;
+};
 
 
 
--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+// #include "includes/geometry.h"
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int row = 20;
+    int col = 10;
+    int wd = row*col;
+
+    // Neurons Grid: frequency or potential at each point
+    double** freqArray = new double*[row];
+    for(int i=0;i < row; i++) {
+        freqArray[i] = new double[col];
+    } 
+
+    // Potential array
+    double** potArray = new double*[row];
+    for(int i=0;i < row; i++) {
+        potArray[i] = new double[col];
+    } 
+
+    // Weight array
+    double** wArray = new double*[wd];
+    for(int i=0; i< wd; i++){
+        wArray[i] = new double[wd];
+    }
+
+    // Test arrays
+    cout << "freqArray[0][0] is " << freqArray[0][0] << endl;
+    cout << "potArray[0][0] is " << potArray[0][0] << endl;
+    cout << "wArray[0][0] is " << wArray[0][0] << endl;
+
+
+
+    cout << "End of Program." << endl;
     return 0;
 }
